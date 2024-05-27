f2876ffcb06ba18af45d5632d06771927a75e181
Steliana-Guta <steliana.m.guta@gmail.com>
2024-05-27 19:02:23 +0100

    Add project files.

---
+++ RetirementPlanner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RetirementPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputAge;
+            //accepts sring for inputAge,all the rest has to be decimals 
+            decimal age;
+            decimal oldAge = 65;
+            decimal difference;
+
+            Console.WriteLine("Please enter your age in whole years");
+            inputAge = Console.ReadLine();
+
+            age = Convert.ToDecimal(inputAge);
+            //i tried to use decimal.parse but itdidnt work,figured it mught work if i converted to decimal,but had to put eveything as decimals first
+         difference = oldAge - age;
+            Console.WriteLine("In {0:0.00} years you will be 65 years old", difference);
+            Console.ReadLine();
+
+        }
+    }
+}
